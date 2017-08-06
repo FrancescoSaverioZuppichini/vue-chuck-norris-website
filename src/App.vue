95a276f8465da6e2ba881fe900e4011b506d3936
VaeVictis <VaeVictis@VaeVictiss-MacBook-Pro.local>
2017-08-06 18:35:03 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+<div id="app" class='d-flex flex-column'>
+  <div class="jumbotron">
+    <h1> Click on Chuck Norris
+    </h1>
+    <h6>to discover funny jokes about him!</h6>
+  </div>
+  <div class="flex--grow"></div>
+  <div class="container">
+    <div class="d-flex justify-content-center">
+      <chuck-norris ></chuck-norris>
+    </div>
+  </div>
+    <div class="flex--grow"></div>
+</div>
+</template>
+
+<script>
+
+import ChuckNorris from 'vue-chuck-norris'
+
+export default {
+  name: 'app',
+  components: {
+    ChuckNorris
+  }
+}
+</script>
+
+<style>
+#app{
+    height: 100vh;
+}
+.flex--grow {
+  flex-grow: 99;
+}
+</style>
